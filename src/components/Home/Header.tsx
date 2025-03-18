7ff788a1f47b924d9af1e45345420a438da98a2f
HamzaBastorDecloedt <hamza.bastor@decloedt.cloud>
2025-03-18 11:38:14 +0000

    SM-449 Change button color

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -16,13 +16,13 @@ const Header: React.FC = () => {
         <div className="mt-5 sm:mt-6 md:mt-10 flex sm:flex-row gap-3 sm:gap-5 justify-center">
           <button
             onClick={() => navigate("")}
-            className="bg-[#A86A28]/[0.7] hover:bg-[#A86A28]/40 text-white border-3 border-[#A9A9A8] px-5 sm:px-4 2xl:px-15 2xl:p-2 md:px-4 py-2 rounded-xl shadow-md transition duration-300 text-md 2xl:text-xl xl:text-md sm:text-lg w-auto max-w-fit whitespace-nowrap cursor-pointer"
+            className="bg-[#d2822d]/[0.7] hover:bg-[#d2822d]/55 text-white border-3 border-[#A9A9A8] px-5 sm:px-4 2xl:px-15 2xl:p-2 md:px-4 py-2 rounded-xl shadow-md transition duration-300 text-md 2xl:text-xl xl:text-md sm:text-lg w-auto max-w-fit whitespace-nowrap cursor-pointer"
           >
             School Admission
           </button>
           <button
             onClick={() => navigate("")}
-            className="bg-[#A86A28]/[0.7] hover:bg-[#A86A28]/40 text-white border-3 border-[#A9A9A8] px-5 sm:px-4 2xl:px-15 2xl:p-2 md:px-4 py-2 rounded-xl shadow-md transition duration-300 text-md 2xl:text-xl xl:text-md sm:text-lg w-auto max-w-fit cursor-pointer whitespace-nowrap"
+            className="bg-[#d2822d]/[0.7] hover:bg-[#d2822d]/55 text-white border-3 border-[#A9A9A8] px-5 sm:px-4 2xl:px-15 2xl:p-2 md:px-4 py-2 rounded-xl shadow-md transition duration-300 text-md 2xl:text-xl xl:text-md sm:text-lg w-auto max-w-fit cursor-pointer whitespace-nowrap"
           >
             Learner Admission
           </button>
@@ -30,7 +30,7 @@ const Header: React.FC = () => {
         <div className="mt-4 sm:mt-6 flex justify-center">
           <button
             onClick={() => navigate("")}
-            className="bg-[#A86A28]/[0.7] hover:bg-[#A86A28]/40 text-white border-3 border-[#A9A9A8] 2xl:mt-4 px-25 2xl:px-52 xl:px-25 lg:px-25 md:px-25 sm:px-25 2xl:p-2 py-2 rounded-xl shadow-md transition duration-300 text-md xl:text-md sm:text-xl w-auto max-w-fit cursor-pointer whitespace-nowrap"
+            className="bg-[#d2822d]/[0.7] hover:bg-[#d2822d]/55 text-white border-3 border-[#A9A9A8] 2xl:mt-4 px-25 2xl:px-52 xl:px-25 lg:px-25 md:px-25 sm:px-25 2xl:p-2 py-2 rounded-xl shadow-md transition duration-300 text-md xl:text-md sm:text-xl w-auto max-w-fit cursor-pointer whitespace-nowrap"
           >
             Discover our courses
           </button>
@@ -47,7 +47,7 @@ const Header: React.FC = () => {
           <div className="mt-3 sm:mt-4 flex justify-center">
             <button
               onClick={() => navigate("")}
-              className="bg-[#A86A28] hover:bg-[#A86A28]/60 text-white border-6 border-[#A9A9A8] px-31 sm:px-40 md:px-35 lg:px-35 py-2 rounded-lg shadow-md transition duration-300 font-semibold text-lg sm:text-lg w-auto max-w-fit cursor-pointer text-nowrap"
+              className="bg-[#A86A28] hover:bg-[#A86A28]/90 text-white border-6 border-[#A9A9A8] px-31 sm:px-40 md:px-35 lg:px-35 py-2 rounded-lg shadow-md transition duration-300 font-semibold text-lg sm:text-lg w-auto max-w-fit cursor-pointer text-nowrap"
             >
               START NOW
             </button>
@@ -66,7 +66,7 @@ const Header: React.FC = () => {
         <div className="mt-3 2xl:mt-7 sm:mt-4 flex justify-center">
           <button
             onClick={() => navigate("")}
-            className="bg-[#A86A28] hover:bg-[#A86A28]/60 text-white border-6 border-[#A9A9A8] px-6 sm:px-10 2xl:px-60 2xl:py-2 xl:px-40 lg:px-16 py-2 rounded-lg shadow-md transition duration-300 font-bold text-sm 2xl:text-xl sm:text-lg w-full sm:w-auto cursor-pointer text-nowrap"
+            className="bg-[#A86A28] hover:bg-[#A86A28]/90 text-white border-6 border-[#A9A9A8] px-6 sm:px-10 2xl:px-60 2xl:py-3 xl:px-40 lg:px-16 py-2 rounded-lg shadow-md transition duration-300 font-bold text-sm 2xl:text-xl sm:text-lg w-full sm:w-auto cursor-pointer text-nowrap"
           >
             START NOW
           </button>
