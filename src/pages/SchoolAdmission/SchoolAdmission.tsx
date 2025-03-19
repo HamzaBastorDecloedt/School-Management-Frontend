c073af220d8f6c07d971d7189df579c4a60a3f14
HamzaBastorDecloedt <hamza.bastor@decloedt.cloud>
2025-03-19 13:04:02 +0000

    SM-453 add school admission page

---
+++ src/pages/SchoolAdmission/SchoolAdmission.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import TopBanner from "../../components/Global/TopBanner";
+import BottomBanner from "../../components/Global/BottomBanner";
+import SchoolAdmissionSection from "../../components/SchoolAdmission/SchoolAdmissionSection";
+
+const CourseDetails: React.FC = () => {
+  return (
+    <>
+        <title>Wayo Academy | School Admission</title>
+        <TopBanner>START YOUR JOURNEY</TopBanner>
+        <SchoolAdmissionSection />
+        <BottomBanner />
+    </>
+  );
+};
+
+export default CourseDetails;
